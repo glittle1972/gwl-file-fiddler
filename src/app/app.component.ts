76563cc9a0ae01b6ee1363083eed89e44d70d24a
Gordon Little <dev@gordonlittle.co.uk>
2020-05-07 17:02:01 +0100

    Basic working version using ngx-dropzone.
    CSS seems to have overlapping components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: [ './app.component.scss' ]
 })
-export class AppComponent {
-  title = 'gwl-file-fiddler';
+export class AppComponent  {
+  
+  authFiles: File[] = [];
+  targetFiles: File[] = [];
+
+	onAuthSelect(event) {
+		console.log('Auth Select: ' + event);
+		this.authFiles.push(...event.addedFiles);
+	}
+
+	onAuthRemove(event) {
+		console.log('Auth Remove: ' + event);
+		this.authFiles.splice(this.authFiles.indexOf(event), 1);
+	}
+
+	onTargetSelect(event) {
+		console.log('Target Select: ' + event);
+		this.targetFiles.push(...event.addedFiles);
+	}
+
+	onTargetRemove(event) {
+		console.log('Target Remove: ' + event);
+		this.targetFiles.splice(this.targetFiles.indexOf(event), 1);
+	}
 }
